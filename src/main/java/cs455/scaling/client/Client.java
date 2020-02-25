91966caf804b2e0d6875ee02cf3a3e079c4c8c8f
nkozakis-csu <gitgudmyguy99>
2020-02-24 23:04:25 -0700

    started testing NIO sockets

--- src/main/java/cs455/scaling/client/Client.java
+++ src/main/java/cs455/scaling/client/Client.java
@@ -1,4 +1,56 @@
 package cs455.scaling.client;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Client {
+	ByteBuffer buf;
+	
+	public Client(){
+		buf = ByteBuffer.allocate(64);
+		
+		
+	}
+	
+	public void connect(){
+		try {
+			SocketChannel socketChannel = SocketChannel.open();
+			socketChannel.connect(new InetSocketAddress("localhost", 50000));
+			if (!socketChannel.finishConnect()){
+				System.out.println("Connection Failed");
+			}
+			System.out.println("Connected");
+			while(socketChannel != null){
+				if(buf.position()==0)
+					buf.put(("data: "+System.currentTimeMillis()).getBytes());
+				buf.flip();
+				socketChannel.write(buf);
+				buf.flip();
+				buf.clear();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public byte[] generateRandomMessage(){
+		return new byte[10];
+	}
+	
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.connect();
+		Scanner scan = new Scanner(System.in);
+		scan.nextLine();
+	}
+	
 }
--- src/main/java/cs455/scaling/server/Server.java
+++ src/main/java/cs455/scaling/server/Server.java
@@ -1,4 +1,49 @@
 package cs455.scaling.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
 public class Server {
+	
+	ByteBuffer buf = ByteBuffer.allocate(64);
+	
+	public Server(){
+
+	}
+	
+	public void listen(){
+		try {
+			ServerSocketChannel sock = ServerSocketChannel.open();
+			sock.socket().bind(new InetSocketAddress(50000));
+			while (true){
+				SocketChannel socketChannel = sock.accept();
+				if (!socketChannel.finishConnect()) {
+					System.out.println("connection failed");
+				}
+				System.out.println("Connection received");
+				while(socketChannel != null){
+					System.out.println("reading channel");
+					int numRead = socketChannel.read(buf);
+					if(numRead>0){
+						System.out.println("Server: "+buf.getChar());
+						buf.clear();
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Done reading");
+	}
+	
+	public static void main(String[] args) {
+		Server s = new Server();
+		s.listen();
+//		Scanner scan = new Scanner(System.in);
+//		scan.nextLine();
+	}
 }
