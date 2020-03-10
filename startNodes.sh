edf9c01956f358a25fb293c9720e564aca5b0c55
nkozakis-csu <gitgudmyguy99>
2020-03-10 15:47:46 -0600

    testing in lab

---
+++ startNodes.sh
@@ -0,0 +1,11 @@
+DIR="$( cd "$( dirname "$0" )" &&pwd )"
+JAR_PATH="$DIR/conf/:$DIR/build/libs/P2P-Network-1.0-SNAPSHOT.jar"
+MACHINE_LIST="$DIR/conf/machine_list"
+SCRIPT="java -cp $JAR_PATH cs455.scaling.server.Server 129.82.44.135 50000"
+COMMAND='gnome-terminal --geometry=200x40'
+for machine in `cat $MACHINE_LIST`
+do
+    OPTION='--tab -t "'$machine'" -e "ssh -t '$machine' cd '$DIR'; echo '$SCRIPT'; '$SCRIPT'"'
+    COMMAND+=" $OPTION"
+done
+eval $COMMAND &
